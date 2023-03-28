864f8b8f1d680342980e8f0b116b70fa08a03520
Aaron Halsell <aaron.halsell@gmail.com>
2023-03-28 16:41:53 -0600

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { TimesheetsComponent } from './components/timesheets/timesheets.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentsComponent,
+    TimesheetsComponent,
+    AnalyticsComponent,
+    TopNavbarComponent,
+    AnalyticsTableComponent
   ],
   imports: [
     BrowserModule,
