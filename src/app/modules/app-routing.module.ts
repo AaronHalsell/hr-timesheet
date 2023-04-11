562b4ebdd86d3fe200a41950947404c38aee43c0
Aaron Halsell <aaron.halsell@gmail.com>
2023-04-11 10:07:57 -0600

    Routing basics

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentsComponent } from '../components/departments/departments.component';
+import { TimesheetsComponent } from '../components/timesheets/timesheets.component';
+import { AnalyticsComponent } from '../components/analytics/analytics.component'
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '',   redirectTo: 'departments', pathMatch: 'full' },
+  { path: 'departments', component: DepartmentsComponent },
+  { path: 'timesheet', component: TimesheetsComponent },
+  { path: 'analytics', component: AnalyticsComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
