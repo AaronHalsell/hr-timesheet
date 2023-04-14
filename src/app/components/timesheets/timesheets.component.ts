ef7a7e85f21f37753e8fa2ba782ed4d7cce1cfe3
Aaron Halsell <aaron.halsell@gmail.com>
2023-04-14 10:08:24 -0600

    additional routing

--- src/app/components/timesheets/timesheets.component.ts
+++ src/app/components/timesheets/timesheets.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service'
 
 @Component({
   selector: 'app-timesheets',
   templateUrl: './timesheets.component.html',
   styleUrls: ['./timesheets.component.scss']
 })
-export class TimesheetsComponent {
+export class TimesheetsComponent implements OnInit {
+  departments: Department[] | undefined;
+  department: Department | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private departmentsService: DepartmentsService,
+  ) { }
+
+ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+    this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+  }
 }
